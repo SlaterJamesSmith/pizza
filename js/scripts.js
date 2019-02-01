2981d3cfb3f66b9c0b3e6afe721c0a6dc0405a5c
Slater Smith <smithslater@gmail.com>
2019-02-01 12:24:38 -0800

    create functioning button

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@ Pizza.prototype.costCalc = function () {
   this.cost = 10 + (this.size * 3) + this.toppings.length;
 }
 
+var newPizza = new Pizza;
 $(document).ready(function () {
-  
+
+  $("#size-button").click(function () {
+    newPizza.size = 12
+
+
+  })
 
 
 
 
 
-}
+});
