960c36cda8d1cb0551dc2e8de7aa2f8baefa5aff
Slater Smith <smithslater@gmail.com>
2019-02-01 09:00:09 -0800

    create constructor function and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-function Pizza (size, []) {
+function Pizza (size, toppings) {
   this.size = size
-  this.toppings = []
+  this.toppings = toppings
+  this.cost = 0
+}
+
+Pizza.prototype.costCalc = function () {
+  this.cost = 10 + (this.size * 3) + this.toppings.length;
 }
